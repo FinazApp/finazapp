32c6a4b458bd76044312ee2cf8969174d4ff4fc6
Console.WriteLine("J") <97796230+msj0se@users.noreply.github.com>
2024-09-18 21:01:10 -0400

    Update cypress config

--- finaz-app.client/cypress.config.cjs
+++ finaz-app.client/cypress.config.cjs
@@ -2,8 +2,12 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'https://localhost:5173/',
+        baseUrl: 'https://localhost:5174/',
         specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
         supportFile: false,
+        chromeWebSecurity: false,
     },
+    env: {
+        apiUrl: 'https://localhost:7111'
+    }
 })
