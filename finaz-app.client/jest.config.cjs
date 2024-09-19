52b0d0b31561a1ae3f7a2536da0e28a7ce221bf0
Console.WriteLine("J") <97796230+msj0se@users.noreply.github.com>
2024-09-18 21:21:04 -0400

    Update jest config

--- finaz-app.client/jest.config.cjs
+++ finaz-app.client/jest.config.cjs
@@ -1,6 +1,12 @@
 /** @type {import('jest').Config} */
 const config = {
     verbose: true,
+    testEnvironment: 'jsdom',
+    testMatch: [
+        '**/__tests__/**/*.[jt]s?(x)',
+        '**/?(*.)+(spec|test).[tj]s?(x)'
+    ],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
 };
 
 module.exports = config;
