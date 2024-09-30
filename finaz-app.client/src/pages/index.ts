426852e9b6292caa6aad2ea4212d4680425ce9a5
Johan Ezequiel Sierra Linares <jsierra@nubeteck.com>
2024-09-30 11:52:27 -0400

    refactor: :recycle: update

--- finaz-app.client/src/pages/index.ts
+++ finaz-app.client/src/pages/index.ts
@@ -1,5 +1,4 @@
 export { BillsPage } from "./bills";
-export { LoginPage } from "./login";
 export { IncomesPage } from "./incomes";
 export { RegisterPage } from "./register";
 export { DashboardPage } from "./dashboard";
