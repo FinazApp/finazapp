386d73c4d98725d3411f9df85b3e43625688e852
Junho Ramos <junhorams@gmail.com>
2024-11-11 13:57:32 -0500

    Agregar endpoints para restaurar datos y obtener datos del usuario autenticado

--- finaz-app.Server/Controllers/CategoriasController.cs
+++ finaz-app.Server/Controllers/CategoriasController.cs
@@ -1,19 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using finaz_app.Server.Models;
 using AutoMapper;
 using finaz_app.Server.Models.DTOs;
 using Microsoft.AspNetCore.Authorization;
+using finaz_app.Server.Security.JWT;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace finaz_app.Server.Controllers
 {
     /// <summary>
     /// Controlador API para la gestión de categorías en FinanzApp.
     /// </summary>
-    /// <remarks>
-    /// Este controlador maneja las operaciones CRUD para las categorías, como listar, obtener, 
-    /// crear, actualizar y eliminar categorías.
-    /// </remarks>
     [Route("api/[controller]")]
     [ApiController]
     [Authorize(Roles = "usuario, admin")]
@@ -22,24 +22,12 @@ namespace finaz_app.Server.Controllers
         private readonly FinanzAppContext _context;
         private readonly IMapper _mapper;
 
-        /// <summary>
-        /// Constructor de CategoriasController.
-        /// </summary>
-        /// <param name="context">Contexto de la base de datos de FinanzApp.</param>
-        /// <param name="mapper">Interfaz de AutoMapper para mapear entidades a DTOs.</param>
         public CategoriasController(FinanzAppContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
 
-        /// <summary>
-        /// Obtiene todas las categorías.
-        /// </summary>
-        /// <returns>Una lista de CategoriasDTO.</returns>
-        /// <response code="200">Devuelve la lista de categorías.</response>
-        /// <response code="401">No autorizado.</response>
-        /// <response code="403">Prohibido para el rol actual.</response>
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<CategoriasDTO>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -48,7 +36,17 @@ namespace finaz_app.Server.Controllers
         {
             try
             {
-                var categorias = await _context.Categorias.Include(g => g.Usuario).ToListAsync();
+                var userID = JwtHelper.ObtenerIdDeJwt(HttpContext);
+                if (userID == null)
+                {
+                    return Unauthorized("No se ha proporcionado un JWT válido o el ID de usuario no es válido.");
+                }
+
+                var categorias = await _context.Categorias
+                    .Where(c => c.CreadoPor == userID || c.CreadoPor == null)
+                    .Where(c => !c.isDeleted)
+                    .ToListAsync();
+
                 var categoriasDTO = _mapper.Map<IEnumerable<CategoriasDTO>>(categorias);
                 return Ok(categoriasDTO);
             }
@@ -58,30 +56,22 @@ namespace finaz_app.Server.Controllers
             }
         }
 
-        /// <summary>
-        /// Obtiene una categoría específica por ID.
-        /// </summary>
-        /// <param name="id">ID de la categoría.</param>
-        /// <returns>Una categoría específica.</returns>
-        /// <response code="200">Devuelve la categoría solicitada.</response>
-        /// <response code="404">Categoría no encontrada.</response>
         [HttpGet("{id}")]
-        [ProducesResponseType(typeof(IEnumerable<CategoriasDTO>), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(CategoriasDTO), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<CategoriasDTO>> GetCategoria(int id)
         {
             try
             {
-                var categoria = await _context.Categorias.FindAsync(id);
-                var categoriasDTO = _mapper.Map<CategoriasDTO>(categoria);
+                var categoria = await _context.Categorias
+                    .SingleOrDefaultAsync(a => a.CategoriaId == id && !a.isDeleted);
 
-                if (categoriasDTO == null)
+                if (categoria == null)
                 {
-                    return NotFound();
+                    return NotFound($"No se encontró la categoría con ID = {id}");
                 }
 
+                var categoriasDTO = _mapper.Map<CategoriasDTO>(categoria);
                 return Ok(categoriasDTO);
             }
             catch (Exception ex)
@@ -90,29 +80,38 @@ namespace finaz_app.Server.Controllers
             }
         }
 
-        /// <summary>
-        /// Actualiza una categoría existente.
-        /// </summary>
-        /// <param name="id">ID de la categoría a actualizar.</param>
-        /// <param name="categoria">Datos de la categoría actualizados.</param>
-        /// <returns>Resultado de la operación.</returns>
-        /// <response code="204">Categoría actualizada correctamente.</response>
-        /// <response code="400">El ID proporcionado no coincide.</response>
-        /// <response code="404">Categoría no encontrada.</response>
         [HttpPatch("{id}")]
-        [Authorize(Roles = "admin")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> PutCategoria(int id, Categoria categoria)
+        public async Task<IActionResult> PutCategoria(int id, [FromBody] Categoria categoria)
         {
             if (id != categoria.CategoriaId)
             {
-                return BadRequest("El ID del usuario no coincide.");
+                return BadRequest("El ID de la categoría no coincide.");
             }
 
-            _context.Entry(categoria).State = EntityState.Modified;
+            var existingCategoria = await _context.Categorias.FindAsync(id);
+            if (existingCategoria == null || existingCategoria.isDeleted)
+            {
+                return NotFound($"No se encontró la categoría con ID = {id}");
+            }
+
+            var userID = JwtHelper.ObtenerIdDeJwt(HttpContext);
+            if (userID == null)
+            {
+                return Unauthorized("No se ha proporcionado un JWT válido o el ID de usuario no es válido.");
+            }
+
+            existingCategoria.isSystem = User.IsInRole("admin");
+
+            existingCategoria.Nombre = categoria.Nombre;
+            existingCategoria.Descripcion = categoria.Descripcion;
+            existingCategoria.ModificadoPor = userID.Value;
+            existingCategoria.FechaModificado = DateTime.UtcNow;
+
+            _context.Entry(existingCategoria).State = EntityState.Modified;
 
             try
             {
@@ -122,11 +121,11 @@ namespace finaz_app.Server.Controllers
             {
                 if (!CategoriaExists(id))
                 {
-                    return NotFound($"No se encontró el usuario con ID = {id}");
+                    return NotFound($"No se encontró la categoría con ID = {id}");
                 }
                 else
                 {
-                    return StatusCode(500, "Ocurrió un error al actualizar el usuario.");
+                    return StatusCode(500, "Ocurrió un error al actualizar la categoría.");
                 }
             }
             catch (Exception ex)
@@ -137,13 +136,6 @@ namespace finaz_app.Server.Controllers
             return NoContent();
         }
 
-        /// <summary>
-        /// Crea una nueva categoría.
-        /// </summary>
-        /// <param name="categoria">Los datos de la nueva categoría.</param>
-        /// <returns>La categoría creada.</returns>
-        /// <response code="201">Categoría creada correctamente.</response>
-        /// <response code="400">Solicitud incorrecta.</response>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -157,30 +149,35 @@ namespace finaz_app.Server.Controllers
 
             try
             {
+                var userID = JwtHelper.ObtenerIdDeJwt(HttpContext);
+                if (userID == null)
+                {
+                    return Unauthorized("No se ha proporcionado un JWT válido o el ID de usuario no es válido.");
+                }
+
+                categoria.isSystem = User.IsInRole("admin");
+
+                categoria.ModificadoPor = userID.Value;
+                categoria.CreadoPor = userID.Value;
+                categoria.FechaCreacion = DateTime.UtcNow;
+                categoria.FechaModificado = DateTime.UtcNow;
+
                 _context.Categorias.Add(categoria);
                 await _context.SaveChangesAsync();
 
-                return CreatedAtAction("GetCategoria", new { id = categoria.CategoriaId }, categoria);
+                return CreatedAtAction(nameof(GetCategoria), new { id = categoria.CategoriaId }, categoria);
             }
-            catch (DbUpdateException dbEx)
+            catch (DbUpdateException dbEx) when (dbEx.InnerException != null)
             {
-                return StatusCode(500, "Error de base de datos al guardar la categoría.");
+                return StatusCode(500, $"Error al guardar la categoría: {dbEx.InnerException.Message}");
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "Ocurrió un error inesperado en el servidor.");
+                return StatusCode(500, $"Ocurrió un error inesperado: {ex.Message}");
             }
         }
 
-        /// <summary>
-        /// Elimina una categoría por ID.
-        /// </summary>
-        /// <param name="id">ID de la categoría a eliminar.</param>
-        /// <returns>Resultado de la operación.</returns>
-        /// <response code="204">Categoría eliminada correctamente.</response>
-        /// <response code="404">Categoría no encontrada.</response>
         [HttpDelete("{id}")]
-        [Authorize(Roles = "admin")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteCategoria(int id)
@@ -188,12 +185,21 @@ namespace finaz_app.Server.Controllers
             try
             {
                 var categoria = await _context.Categorias.FindAsync(id);
-                if (categoria == null)
+                if (categoria == null || categoria.isDeleted)
                 {
-                    return NotFound();
+                    return NotFound($"No se encontró la categoría con ID = {id}");
                 }
 
-                _context.Categorias.Remove(categoria);
+                var userID = JwtHelper.ObtenerIdDeJwt(HttpContext);
+                if (userID == null)
+                {
+                    return Unauthorized("No se ha proporcionado un JWT válido o el ID de usuario no es válido.");
+                }
+
+                categoria.ModificadoPor = userID.Value;
+                categoria.FechaModificado = DateTime.UtcNow;
+                categoria.isDeleted = true;
+                _context.Entry(categoria).State = EntityState.Modified;
                 await _context.SaveChangesAsync();
 
                 return NoContent();
@@ -204,14 +210,77 @@ namespace finaz_app.Server.Controllers
             }
         }
 
-        /// <summary>
-        /// Verifica si una categoría existe.
-        /// </summary>
-        /// <param name="id">ID de la categoría a verificar.</param>
-        /// <returns>True si la categoría existe, de lo contrario false.</returns>
         private bool CategoriaExists(int id)
         {
-            return _context.Categorias.Any(e => e.CategoriaId == id);
+            return _context.Categorias.Any(e => e.CategoriaId == id && !e.isDeleted);
+        }
+
+        [HttpPost("restore")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> RestoreUserCategories()
+        {
+            var userID = JwtHelper.ObtenerIdDeJwt(HttpContext);
+            if (userID == null)
+            {
+                return Unauthorized("No se ha proporcionado un JWT válido o el ID de usuario no es válido.");
+            }
+
+            try
+            {
+                // Restaurar categorías, gastos e ingresos del usuario que estén marcados como eliminados
+                var categorias = await _context.Categorias
+                    .Where(c => c.CreadoPor == userID && c.isDeleted)
+                    .ToListAsync();
+                var gastos = await _context.Gastos
+                    .Where(g => g.CreadoPor == userID && g.isDeleted)
+                    .ToListAsync();
+                var ingresos = await _context.Ingresos
+                    .Where(i => i.CreadoPor == userID && i.isDeleted)
+                    .ToListAsync();
+
+                // Cambiar el estado de 'isDeleted' a falso
+                categorias.ForEach(c => c.isDeleted = false);
+                gastos.ForEach(g => g.isDeleted = false);
+                ingresos.ForEach(i => i.isDeleted = false);
+
+                await _context.SaveChangesAsync();
+                return Ok("Categorías, ingresos y gastos restaurados correctamente.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error en la API: {ex.Message}");
+            }
+        }
+
+        [HttpGet("user/me")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetAuthenticatedUser()
+        {
+            var userID = JwtHelper.ObtenerIdDeJwt(HttpContext);
+            if (userID == null)
+            {
+                return Unauthorized("No se ha proporcionado un JWT válido o el ID de usuario no es válido.");
+            }
+
+            var user = await _context.Usuarios
+                .AsNoTracking() // Para optimizar si no se va a modificar el objeto
+                .FirstOrDefaultAsync(u => u.UsuarioId == userID);
+
+            if (user == null)
+            {
+                return NotFound("Usuario no encontrado.");
+            }
+
+            return Ok(new {
+                user.UsuarioId,
+                user.Nombre,
+                user.CorreoElectronico,
+                user.Rol
+            });
         }
     }
 }
