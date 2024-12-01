286b452e2c6bef4ac89aaf1edacccf24ab4ae2b4
SmillRivera14 <smillriveradiaz14@gmail.com>
2024-12-01 14:19:25 -0400

    Peticiones para el dasbhoard

---
+++ finaz-app.Server/Controllers/DashboardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace finaz_app.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+    }
+}
